acdd22b73113ce58716764d8ff513aa652923c60
Rayyan Ul Haq <ru03588@st.habib.edu.pk>
2021-02-09 11:10:18 +0500

    EA updated

--- EA.py
+++ EA.py
@@ -5,29 +5,55 @@ import matplotlib.pyplot as plt
 
 
 class EA:
-    def __init__(problem, parentSelection, survivorSelection):
+    def __init__(self, problem, parentSelection, survivorSelection):
         self.problem = problem
+        self.parentSelection = parentSelection
+        self.survivorSelection = survivorSelection
 
-    def fitness(individual):
-        self.problem.fitness(individual)
+    def fitness(self, individual):
+        return self.problem.fitness(individual)
         
-    def crosover(individual1, individual2):
-        self.problem.crosover(individual1, individual2)
+    def crosover(self, individual1, individual2):
+        return newIndividual = self.problem.crosover(individual1, individual2)
 
-    def mutation(individual):
-        if random.random() < self.mutationRate:
-            self.problem.mutation(individual)
+    def mutation(self, individual):
+        if random.random() < self.mutRate:
+            individual = self.problem.mutation(individual)
+        return individual
+        
+    def genFitness(self);
+        return [self.fitness(i) for i in self.generation]
+        
 
-    def runOnce(popSize, numGen, mutRate, numOffSpring):
+    def runOnce(self, popSize, numGen, mutRate, numOffSpring):
         self.popSize = popSize
         self.numGen = numGen
         self.mutRate = mutRate
         self.numOffSpring = numOffSpring
         
-        self.problem.
+        genWiseFitness = []
+        self.generation = self.problem.initializePopulation(popSize)
+        genWiseFitness.append(self.genFitness())
+        
+        for gen in range(numGen):
+            for offS in range(numOffSpring):
+                parents = self.parentSelection(2)
+                newIndividual = self.crosover(parents[0], parents[2])
+                newIndividual = self.mutation(newIndividual)
+                self.generation.append(newIndividual)
+            
+            self.generation = self.survivorSelection(numGen)
+            genWiseFitness.append(self.genWiseFitness())
+        
+        return genWiseFitness
+        
     
     def runKTimes(popSize, numGen, mutRate, numOffSpring, iterations):
-        pass
+        iterWiseFitness = []
+        for i in range(iterations):
+            iterWiseFitness.append(self.runOnce(popSize, numGen, mutRate, numOffSpring)
+        return iterWiseFitness
+            
 
 
-EA(problem, f.binaryTrounamaent())
+EA(tsp(filename), f.binaryTrounamaent)
