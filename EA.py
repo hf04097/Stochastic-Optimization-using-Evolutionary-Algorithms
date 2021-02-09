de5f08a941a04083473ddab69813678e8ac5b5a4
Rayyan Ul Haq <ru03588@st.habib.edu.pk>
2021-02-09 12:12:44 +0500

    completed EA i think

--- EA.py
+++ EA.py
@@ -36,26 +36,33 @@ class EA:
         genWiseFitness.append(self.genFitness())
         
         for gen in range(numGen):
-            parents = self.parentSelection(numOffSpring * 2)
+            parents = self.parentSelection(generation, numOffSpring * 2)
             for i in range(0, numOffSpring, 2):                
                 newIndividual = self.crosover(parents[i], parents[i+1])
                 newIndividual = self.mutation(newIndividual)
                 self.generation.append(newIndividual)
             
-            self.generation = self.survivorSelection(numGen)
+            self.generation = self.survivorSelection(generation, numGen)
             genWiseFitness.append(self.genWiseFitness())
         
         genWiseFitness = np.array(genWiseFitness)
-        
+        genWiseFitnessAvgs = genWiseFitness.mean(axis = 1)
+        genWiseFitnessMaxs = np.maximum.accumulate(genWiseFitness, axis = 1)
+        return genWiseFitnessAvgs, genWiseFitnessMaxs
         
     
     def runKTimes(popSize, numGen, mutRate, numOffSpring, iterations):
-        iterWiseFitness = []
+        iterWiseFitnessAvgs = []
+        iterWiseFitnessMaxs = []
         for i in range(iterations):
-            iterWiseFitness.append(self.runOnce(popSize, numGen, mutRate, numOffSpring)
-        iterWiseFitness = np.array(genWiseFitness)
-        iterWiseAvgs = iterWiseFitness.mean(axis = 0)
-        plt.plot(
+            genWiseFitnessAvgs, genWiseFitnessMaxs = self.runOnce(popSize, numGen, mutRate, numOffSpring)
+            iterWiseFitnessAvgs.append(genWiseFitnessAvgs)
+            iterWiseFitnessMaxs.append(genWiseFitnessMaxs)
+        iterWiseFitnessAvgs = np.array(iterWiseFitnessAvgs)
+        iterWiseFitnessMaxs = np.array(iterWiseFitnessMaxs)
+        iterWiseAvgs = iterWiseFitnessAvgs.mean(axis = 0)
+        iterWiseMaxs = np.maximum.accumulate(iterWiseFitnessMaxs, axis = 0)
+        plt.plot(range(len(iterWiseAvgs)), iterWiseAvgs)
             
 
 
