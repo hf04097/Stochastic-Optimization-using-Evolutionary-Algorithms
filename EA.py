a3d25c1558108b0bde35e1603ccb58ab6dce7928
hf04097 <hf04097@st.habib.edu.pk>
2021-02-10 20:49:32 +0500

    plot saving added and results folder added

--- EA.py
+++ EA.py
@@ -15,7 +15,7 @@ class EA:
 
     def fitness(self, individual):
         return self.problem.Fitness(individual)
-        
+
     def crosover(self, individual1, individual2):
         newIndividual = self.problem.Crossover(individual1, individual2)
         return newIndividual
@@ -24,40 +24,38 @@ class EA:
         if random.random() < self.mutRate:
             individual = self.problem.Mutation(individual)
         return individual
-        
+
     def genFitness(self):
         return [self.fitness(i) for i in self.generation]
-        
 
     def runOnce(self, popSize, numGen, mutRate, numOffSpring):
         self.popSize = popSize
         self.numGen = numGen
         self.mutRate = mutRate
         self.numOffSpring = numOffSpring
-        
+
         genWiseFitness = []
         self.generation = self.problem.initializePopulation(popSize)
         genWiseFitness.append(self.genFitness())
-        
+
         for gen in range(numGen):
             parents = self.parentSelection(self.generation, numOffSpring * 2)
             # print(len(parents))
             for i in range(0, numOffSpring * 2, 2):
-                newIndividual = self.crosover(parents[i], parents[i+1])
+                newIndividual = self.crosover(parents[i], parents[i + 1])
                 newIndividual = self.mutation(newIndividual)
                 self.generation.append(newIndividual)
             # print(len(self.generation), "yahan", popSize)
             self.generation = self.survivorSelection(self.generation, popSize)
             # print(len(self.generation), popSize, " ye wala")
             genWiseFitness.append(self.genFitness())
-        
+
         genWiseFitness = np.array(genWiseFitness)
-        genWiseFitnessAvgs = genWiseFitness.mean(axis = 1)
-        genWiseFitnessMaxs = np.maximum.accumulate(genWiseFitness.max(axis = 1), axis = 0)
+        genWiseFitnessAvgs = genWiseFitness.mean(axis=1)
+        genWiseFitnessMaxs = np.maximum.accumulate(genWiseFitness.max(axis=1), axis=0)
         return genWiseFitnessAvgs, genWiseFitnessMaxs
-        
-    
-    def runKTimes(self, popSize = 30, numGen = 100, mutRate = 0.5, numOffSpring = 10, iterations = 10):
+
+    def runKTimes(self, popSize=30, numGen=100, mutRate=0.5, numOffSpring=10, iterations=10):
         iterWiseFitnessAvgs = []
         iterWiseFitnessMaxs = []
         for i in range(iterations):
@@ -66,16 +64,20 @@ class EA:
             iterWiseFitnessMaxs.append(genWiseFitnessMaxs)
         iterWiseFitnessAvgs = np.array(iterWiseFitnessAvgs)
         iterWiseFitnessMaxs = np.array(iterWiseFitnessMaxs)
-        iterWiseAvgs = iterWiseFitnessAvgs.mean(axis = 0)
-        iterWiseMaxs = np.maximum.accumulate(iterWiseFitnessMaxs, axis = 1).max(axis = 0)
+        iterWiseAvgs = iterWiseFitnessAvgs.mean(axis=0)
+        iterWiseMaxs = np.maximum.accumulate(iterWiseFitnessMaxs, axis=1).max(axis=0)
         plt.plot(range(numGen + 1), iterWiseAvgs)
         plt.xlabel("generations")
         plt.ylabel("average across iterations")
+        plt.savefig('results/tsp/' + 'pop_' + str(popSize) + '_gen_' + str(numGen) + '_mut_' + str(
+            mutRate * 100) + '_offspring_' + str(
+            numOffSpring) + 'iter_' + str(iterations) + '.png')
         plt.show()
-        
+
+
 
 population = 30
-numGen = 100
+numGen = 5
 mutRate = 0.5
 numOffSpring = 100
 iterations = 10
@@ -87,8 +89,3 @@ selobj = Selection(problem)
 
 eaobject = EA(problem, selobj.BinaryTournamentSelection, selobj.FitnessProportionalSelection)
 eaobject.runKTimes(population, numGen, mutRate, numOffSpring, iterations)
-
-
-
-
-
--- selection.py
+++ selection.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 
+
 class Selection:
     def __init__(self, problem):
         self.selected = []
@@ -17,12 +18,12 @@ class Selection:
             self.selected.append(population[np.random.choice(len(population), 1, p=selection_prob).tolist()[0]])
         return self.selected
 
-    def RankBasedSelection(self):
-        worst_sorted_fitness = sorted(self.population, key=lambda agent: self.problem.Fitness(agent),reverse=True)
+    def RankBasedSelection(self, population, selection_size):
+        worst_sorted_fitness = sorted(population, key=lambda agent: self.problem.Fitness(agent), reverse=True)
         ranks = np.arange(len(worst_sorted_fitness))
         selection_prob = [r / sum(ranks) for r in ranks]
-        for i in range(self.selection_size):
-            self.selected.append( worst_sorted_fitness[np.random.choice(len(worst_sorted_fitness), p=selection_prob)])
+        for i in range(selection_size):
+            self.selected.append(worst_sorted_fitness[np.random.choice(len(worst_sorted_fitness), p=selection_prob)])
         return self.selected
 
     def BinaryTournamentSelection(self, population, selection_size):
@@ -33,12 +34,12 @@ class Selection:
             self.selected.append(sorted_binary[0])
         return self.selected
 
-    def TruncationSelection(self):
+    def TruncationSelection(self, population, selection_size):
         truncation_threshold = 0.5
-        trunc = sorted(self.population, key=lambda agent: self.problem.Fitness(agent), reverse=True)[
-                :int(len(self.population) * truncation_threshold)]
-        for i in range(self.selection_size):
+        trunc = sorted(population, key=lambda agent: self.problem.Fitness(agent), reverse=True)[
+                :int(len(population) * truncation_threshold)]
+        for i in range(selection_size):
             self.selected.append(trunc[random.randint(0, len(trunc) - 1)])
 
-    def RandomSelection(self):
-        return random.sample(self.population, self.selection_size)
+    def RandomSelection(self, population, selection_size):
+        return random.sample(population, selection_size)
