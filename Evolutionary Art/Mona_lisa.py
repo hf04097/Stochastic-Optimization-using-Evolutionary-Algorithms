478ad694c2f30dbb483b6209dc1866f44a51c531
hf04097 <hf04097@st.habib.edu.pk>
2021-02-12 21:45:07 +0500

    gene class added

--- Evolutionary Art/Mona_lisa.py
+++ Evolutionary Art/Mona_lisa.py
@@ -6,7 +6,23 @@ import cv2
 import math
 import os
 
-class Gene():
-    def __init__(self,max_vertices):
-        self.number_vertices = random.randrange(2,max_vertices)
 
+class Gene():
+    def __init__(self, actual_image, max_coordinate):
+        self.number_coordinate = random.randrange(2, max_coordinate)
+        self.coordinate = []
+        self.actual_image = actual_image.convert('RGBA')
+
+    def random_coordinate(self):
+        for i in range(self.number_coordinate):
+            self.coordinate.append(
+                (random.uniform(0, self.actual_image.size[0]), random.uniform(0, self.actual_image.size[1])))
+            return self.coordinate
+
+    def random_color(self):
+        return tuple([random.randint(0, 255) for i in range(4)])
+
+
+img = Image.open("mona_image.jpg")
+gene = Gene(img, 4)
+print(gene.random_coordinate())
